ddc494f344fcb0de88aae3a537f0d8ed16060019
adel-galyameev <112763316+adel-galyameev@users.noreply.github.com>
2022-12-17 15:32:24 +0300

    final1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -16,17 +16,17 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         UserService us = new UserServiceImpl();
 
-        //us.createUsersTable();
+        us.createUsersTable();
 
         us.saveUser("Bob","Smeet",(byte) 20);
         us.saveUser("Tom","Black",(byte) 25);
         us.saveUser("Slim","Blue",(byte) 40);
         us.saveUser("Rob","Smail",(byte) 99);
-//
-        //us.getAllUsers();
 
-        //us.cleanUsersTable();
+        us.getAllUsers();
 
-       // us.dropUsersTable();
+        us.cleanUsersTable();
+
+        us.dropUsersTable();
     }
 }
